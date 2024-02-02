1d4b7d0a163bb3a6ffaec57a0deb70aeabb1a3dc
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-02-02 13:19:21 +0200

    update events route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth');
+const eventsRoute = require('./routes/events');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/events', eventsRoute);
 
 // Sample destinations config
 const destinationsConfig = {
@@ -14,31 +21,5 @@ const destinationsConfig = {
     fallback: 'http://fallback.com',
   }
 };
-
-// Endpoint to receive events
-app.post('/events', (req, res) => {
-  const eventData = req.body;
-
-  // Determine routing strategy
-  const routingStrategy = req.query.strategy || destinationsConfig.strategy;
-
-  // Determine destinations based on strategy
-  const destinations = destinationsConfig.destinations[routingStrategy] || destinationsConfig.destinations.fallback;
-
-  // Send events to destinations
-  sendEvents(eventData, destinations);
-
-  res.status(200).send('Event received successfully.');
-});
-
-// Function to send events to destinations
-function sendEvents(eventData, destinations) {
-  destinations.forEach((destination) => {
-    // Implement logic to send events (e.g., using HTTP requests)
-    // You might want to use a library like axios for making HTTP requests
-    // Example: axios.post(destination, eventData);
-    console.log(`Sending event to ${destination}`);
-  });
-}
 
 module.exports = app;
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,30 @@
 const express = require('express');
   const router = express.Router();
   const verifyToken = require('../middleware/authMiddleware');
+
+
+// Function to send events to destinations
+function sendEvents(eventData, destinations) {
+  destinations.forEach((destination) => {
+    // Implement logic to send events (e.g., using HTTP requests)
+    // You might want to use a library like axios for making HTTP requests
+    // Example: axios.post(destination, eventData);
+    console.log(`Sending event to ${destination}`);
+  });
+}
+
 // Protected route
   router.get('/', verifyToken, (req, res) => {
-  res.status(200).json({ message: 'Protected route accessed' });
+  // Determine routing strategy
+  const routingStrategy = req.query.strategy || destinationsConfig.strategy;
+
+  // Determine destinations based on strategy
+  const destinations = destinationsConfig.destinations[routingStrategy] || destinationsConfig.destinations.fallback;
+
+   // Send events to destinations
+  sendEvents(eventData, destinations);
+
+  res.status(200).json('Event received successfully.');
 });
 
 module.exports = router;
