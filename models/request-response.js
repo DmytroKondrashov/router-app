dcce22e2d5ca80699a1641cc8614507157612228
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-02-04 19:20:12 +0200

    delete request-response model

--- models/request-response.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const requestResponseSchema = new mongoose.Schema({
-  request: { type: Object, required: true },
-  response: { type: Object, required: true },
-});
-module.exports = mongoose.model('RequestResponse', requestResponseSchema);
