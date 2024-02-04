4989b2b1f90e1c0704689a617ca5eda14eeca837
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-02-04 20:02:07 +0200

    add requests logger

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const eventsRoute = require('./routes/events');
+const logMiddleware = require('./middleware/logMiddleware')
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(logMiddleware);
 app.use('/auth', authRoutes);
 app.use('/events', eventsRoute);
 
---
+++ middleware/logMiddleware.js
@@ -0,0 +1,36 @@
+const Log = require('../models/log');
+
+const logMiddleware = async (req, res, next) => {
+  console.log('MIDDLEWARE TRIGGERED!')
+  const log = new Log({
+    method: req.method,
+    path: req.path,
+    request: {
+      headers: req.headers,
+      body: req.body,
+    },
+  });
+
+  try {
+    const originalSend = res.send;
+    res.send = function (body) {
+      log.response = {
+        statusCode: res.statusCode,
+        body,
+      };
+
+      log.save();
+
+      res.send = originalSend;
+
+      originalSend.apply(res, arguments);
+    };
+
+    next();
+  } catch (error) {
+    console.error('Error in logMiddleware:', error);
+    next();
+  }
+};
+
+module.exports = logMiddleware;
